b07f6247a444dd4ba5912b97a66cc3b3685c6489
Panda.xyz <98690785+Pandaxyz-xd@users.noreply.github.com>
2022-03-20 17:41:26 +0100

    Add files via upload

---
+++ GO/GO Examples - Pandaxyz/text.go
@@ -0,0 +1,9 @@
+// By: Panda.xyz
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World! | Mwaramutse Isi!")
+}
---
+++ GO/iniciar.md
@@ -0,0 +1,7 @@
+# Como iniciar un archivo .go
+
+```go
+go run archivo.go
+```
+
+<p> Recuerda abrir la terminal </p>
